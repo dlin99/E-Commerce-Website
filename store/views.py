c2ded45348196b0e14c98a3721a730737bfa5070
Danny Lin <linycdanny@gmail.com>
2020-12-07 00:49:39 +0800

    cart functionality for authenticated user

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import *
 import json
+import datetime 
 # Create your views here.
 
 def store(request):
@@ -75,3 +76,34 @@ def updateItem(request):
 		orderItem.delete()
 
 	return JsonResponse('Item was added', safe=False)
+
+
+def processOrder(request):
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
+
+	if request.user.is_authenticated:
+		customer = request.user.customer 
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		total = float(data['form']['total'])
+		order.transaction_id = transaction_id
+
+		# avoid user manipulating total
+		if total == order.get_cart_total:
+			order.complete = True
+		order.save()
+
+		if order.shipping == True:
+			ShippingAddress.objects.create(
+				customer=customer,
+				order=order, 
+				address=data['shipping']['address'],
+				city=data['shipping']['city'],
+				state=data['shipping']['state'],
+				zipcode=data['shipping']['zipcode']
+				)
+
+	else:
+		print('User is not logged in...')
+
+	return JsonResponse('Payment submitted...', safe=False)
