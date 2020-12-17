c25397eff1120b87991c30e9116ddd80b9b678c2
Danny Lin <linycdanny@gmail.com>
2020-12-17 21:01:49 +0800

    add login, logout, hello user on main.html

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -136,3 +136,11 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 # Setup send email
 DEFAULT_FROM_EMAIL = 'pocof445@gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = 'True' 
+# EMAIL_HOST_USER = '********'
+# EMAIL_HOST_PASSWORD = '********' # change to *** before uploading
