0682797b0867db8b8cb67fe1b853b373c9d28ff7
Danny Lin <linycdanny@gmail.com>
2020-12-19 10:36:45 +0800

    Updated Settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -20,11 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vx8x*8z-&r9(*3u!p^km60aj^(q$zkese9to7*60#*%+c8qpa&'
+
+# SECRET_KEY = 'vx8x*8z-&r9(*3u!p^km60aj^(q$zkese9to7*60#*%+c8qpa&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
 
+# ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['dlin99djangoapp.herokuapp.com']
 
 
