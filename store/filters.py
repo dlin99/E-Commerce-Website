eb99f66853baf4f58b743ec70feedf118f70d37a
Danny Lin <linycdanny@gmail.com>
2020-12-21 14:41:16 +0800

    added django-filter search-bar search-page

--- store/filters.py
+++ store/filters.py
@@ -6,7 +6,7 @@ from django.forms.widgets import TextInput
 class ProductFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(
     	lookup_expr='icontains',
-    	widget=TextInput(attrs={'placeholder': 'Search Product'})
+    	widget=TextInput(attrs={'placeholder': 'Search Product...'})
     	)
 
     class Meta:
